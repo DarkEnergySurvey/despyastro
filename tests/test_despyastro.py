d76636b6c5a53013d0f3d5ad2746bfa2390db671
Doug Friedel <friedel@illinois.edu>
2020-02-27 15:42:38 -0600

    next round of tests

--- tests/test_despyastro.py
+++ tests/test_despyastro.py
@@ -12,6 +12,7 @@ from contextlib import contextmanager
 from collections import OrderedDict
 from io import StringIO
 from mock import patch
+import numpy as np
 
 from despyastro import coords as cds
 
@@ -76,6 +77,111 @@ class TestCoords(unittest.TestCase):
         self.assertAlmostEqual(long, back_long[0], 5)
         self.assertAlmostEqual(lat, back_lat[0], 5)
 
+    def test_eq2gal(self):
+        long = 25.0
+        lat = 15.0
+        new_long, new_lat = cds.eq2gal(long, lat)
+        self.assertNotAlmostEqual(long, new_long[0], 5)
+        self.assertNotAlmostEqual(lat, new_lat[0], 5)
+        back_long, back_lat = cds.gal2eq(new_long, new_lat)
+        self.assertAlmostEqual(long, back_long[0], 5)
+        self.assertAlmostEqual(lat, back_lat[0], 5)
+
+    def test_eq2gal_1950(self):
+        long = 25.0
+        lat = 15.0
+        new_long, new_lat = cds.eq2gal(long, lat, True)
+        self.assertNotAlmostEqual(long, new_long[0], 5)
+        self.assertNotAlmostEqual(lat, new_lat[0], 5)
+        back_long, back_lat = cds.gal2eq(new_long, new_lat, True)
+        self.assertAlmostEqual(long, back_long[0], 5)
+        self.assertAlmostEqual(lat, back_lat[0], 5)
+
+    def test_eq2ec(self):
+        long = 25.0
+        lat = 15.0
+        new_long, new_lat = cds.eq2ec(long, lat)
+        self.assertNotAlmostEqual(long, new_long[0], 5)
+        self.assertNotAlmostEqual(lat, new_lat[0], 5)
+        back_long, back_lat = cds.ec2eq(new_long, new_lat)
+        self.assertAlmostEqual(long, back_long[0], 5)
+        self.assertAlmostEqual(lat, back_lat[0], 5)
+
+    def test_eq2ec_1950(self):
+        long = 25.0
+        lat = 15.0
+        new_long, new_lat = cds.eq2ec(long, lat, True)
+        self.assertNotAlmostEqual(long, new_long[0], 5)
+        self.assertNotAlmostEqual(lat, new_lat[0], 5)
+        back_long, back_lat = cds.ec2eq(new_long, new_lat, True)
+        self.assertAlmostEqual(long, back_long[0], 5)
+        self.assertAlmostEqual(lat, back_lat[0], 5)
+
+    def test_ec2gal(self):
+        long = 25.0
+        lat = 15.0
+        new_long, new_lat = cds.ec2gal(long, lat)
+        self.assertNotAlmostEqual(long, new_long[0], 5)
+        self.assertNotAlmostEqual(lat, new_lat[0], 5)
+        back_long, back_lat = cds.gal2ec(new_long, new_lat)
+        self.assertAlmostEqual(long, back_long[0], 5)
+        self.assertAlmostEqual(lat, back_lat[0], 5)
+
+    def test_ec2gal_1950(self):
+        long = 25.0
+        lat = 15.0
+        new_long, new_lat = cds.ec2gal(long, lat, True)
+        self.assertNotAlmostEqual(long, new_long[0], 5)
+        self.assertNotAlmostEqual(lat, new_lat[0], 5)
+        back_long, back_lat = cds.gal2ec(new_long, new_lat, True)
+        self.assertAlmostEqual(long, back_long[0], 5)
+        self.assertAlmostEqual(lat, back_lat[0], 5)
+
+    def test_thetaphi2xyz(self):
+        theta = 1.225
+        phi = -.25
+        x, y, z = cds._thetaphi2xyz(theta, phi)
+
+        new_theta, new_phi = cds._xyz2thetaphi(x, y, z)
+
+        self.assertAlmostEqual(theta, new_theta, 5)
+        self.assertAlmostEqual(phi, new_phi, 5)
+
+    def test_eq2xyz(self):
+        long = 25.0
+        lat = 15.0
+        x, y, z = cds.eq2xyz(long, lat)
+
+        back_long, back_lat = cds.xyz2eq(x, y, z)
+        self.assertAlmostEqual(long, back_long[0], 5)
+        self.assertAlmostEqual(lat, back_lat[0], 5)
+
+    def test_sphdist(self):
+        dist = cds.sphdist(15., -15., 25., 30.)
+        self.assertAlmostEqual(46.020717, dist[0], 5)
+
+    def test_gcirc(self):
+        dist = cds.gcirc(15.,-15.,25.,30.)
+        self.assertAlmostEqual(dist[0], 0.803213, 5)
+        dist = cds.gcirc(15.,-15.,25.,30., True)
+        self.assertAlmostEqual(dist[0][0], 0.803213, 5)
+        self.assertAlmostEqual(dist[1][0], 1.360259, 5)
+
+    def test_atbound(self):
+        low = 0.
+        hi = 360.
+        d = np.array(range(-100,400, 10), dtype='f8')
+        print(d)
+        self.assertTrue(len(np.where(d < low)[0]) > 0)
+        self.assertTrue(len(np.where(d > hi)[0]) > 0)
+        cds.atbound(d, low, hi)
+        print(d)
+        self.assertTrue(len(np.where(d < low)[0]) == 0)
+        self.assertTrue(len(np.where(d > hi)[0]) == 0)
+
+        new_d = copy.deepcopy(d)
+        cds.atbound(d, low, hi)
+        self.assertTrue(d.all() == new_d.all())
 
 if __name__ == '__main__':
     unittest.main()
